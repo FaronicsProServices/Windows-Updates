b97c7549a95b6763765344099c844ba9ca7dc27d
FaronicsProServices <proservices@faronics.com>
2025-08-19 20:12:16 +0530

    Create Windows11Upgradetest5.ps1

---
+++ Windows11Upgradetest5.ps1
@@ -0,0 +1,11 @@
+$workingdir = "C:\temp"
+$url = "https://go.microsoft.com/fwlink/?linkid=2171764"
+$file = "$($workingdir)\Win11Upgrade.exe"
+
+If (!(Test-Path $workingdir)) {
+    New-Item -ItemType Directory -Force -Path $workingdir
+}
+
+Invoke-WebRequest -Uri $url -OutFile $file
+
+Start-Process -FilePath $file -ArgumentList "/Install /MinimizeToTaskBar /QuietInstall /SkipEULA /copylogs $workingdir"
