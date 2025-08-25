2f03cbbe36cb26ec5e96c6f97c6d9511f2d2fb29
FaronicsProServices <proservices@faronics.com>
2025-08-25 23:59:38 +0530

    Delete upgrade14.ps1

--- upgrade14.ps1
+++
@@ -1,3 +0,0 @@
-Set-ExecutionPolicy Unrestricted -Scope Process -Force
-
-Start-Process -FilePath 'C:\Win11Upgrade\setup.exe' -ArgumentList '/quiet', '/noreboot', '/dynamicupdate disable', '/showoobe none', '/eula accept' -Verb RunAs -Wait
